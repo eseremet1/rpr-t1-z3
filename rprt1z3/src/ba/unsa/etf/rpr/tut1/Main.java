b93a7517245ab573ee045b786f9689134604ccba
eseremet1 <eseremet1@etf.unsa.ba>
2019-10-14 14:38:08 +0200

    Initial commit

---
+++ rprt1z3/src/ba/unsa/etf/rpr/tut1/Main.java
@@ -0,0 +1,18 @@
+package ba.unsa.etf.rpr.tut1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Sat s = new Sat(2,3,4);
+        s.Ispisi();
+        s.Ispisi();
+        s.Sljedeci();
+        s.Ispisi();
+        s.PomjeriZa(-48);
+        s.Ispisi();
+        s.Postavi(0,0,0);
+        s.Ispisi();
+
+    }
+}
